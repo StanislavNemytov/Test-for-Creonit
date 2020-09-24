a59c4732cda0a4c593601dba59c98495430d5075
Stanislav Nemytov <staninspain69@gmail.com>
2020-09-24 12:02:50 +0200

    Task done.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,56 @@
 const tags = [
   { title: "Крутой", id: 1 },
   { title: "Модный", id: 2 },
-  { title: "Молодёжный", id: 3 }
+  { title: "Молодёжный", id: 3 },
 ];
 const categories = [
   { title: "Кат 1", count: 0, tagId: 1 },
   { title: "Кат 2", count: 5, tagId: 2 },
   { title: "Кат 3", count: 13 },
   { title: "Кат 4", count: 0, tagId: 3 },
-  { title: "Кат 5", count: 2, tagId: 3 }
+  { title: "Кат 5", count: 2, tagId: 3 },
 ];
 
 // Где-то здесь пишем код
 let result = [];
 
+const categoryDom = document.createElement("div");
+const xmp = document.querySelector("xmp");
+
+const createResults = () => {
+  result.push(
+    ...[...categories].filter((category) => {
+      if (category.count) {
+        const { tagId } = category;
+        console.log("createResults -> tagId", tagId);
+        if (tagId) {
+          category.tag = tags.find((tag) => tag.id === tagId);
+        }
+        delete category.tagId;
+        return category;
+      }
+    })
+  );
+
+  result = result.map((category, index) => {
+    const newDomElement = categoryDom.cloneNode(true);
+    console.log("createResults -> newDomElement", newDomElement);
+    newDomElement.className = `category category-${category.title}`;
+    newDomElement.innerHTML = `${category.title}${
+      category.tag ? ", #" + category.tag.title : ''
+    }`;
+
+    return newDomElement;
+  });
+  xmp.after(...result);
+};
+
+createResults();
+
 const resultGolden = [
   { title: "Кат 2", count: 5, tag: { title: "Модный", id: 2 } },
   { title: "Кат 3", count: 13 },
-  { title: "Кат 5", count: 2, tag: { title: "Молодёжный", id: 3 } }
+  { title: "Кат 5", count: 2, tag: { title: "Молодёжный", id: 3 } },
 ];
 
 document.body.insertAdjacentHTML(
